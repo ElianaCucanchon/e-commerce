c18c259d79178fcbdd49f158a022447dda6d4fce
ElianaCucanchon <elianacucanchon@gmail.com>
2024-02-01 16:57:43 -0500

    Actualizacion

--- e-commerceStore/src/App.jsx
+++ e-commerceStore/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';  //Esto ya no es necesario
 import './styles/App.css'
-import { NavBar } from './components/NavBar';
+import { NavBar } from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { CartWidget } from './components/CartWidget/CartWidget';
+
 
 export const App = () => {
   return (
     <>
-      < NavBar mensaje={"Nav BAR"}/>
+      < NavBar/>
+      < CartWidget/>
     </>
   );
 }
---
+++ e-commerceStore/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+
+
+export const CartWidget = () => {
+    return (
+        <>
+            <i className="bi bi-cart3"></i>
+
+        </>
+    );
+}
+
+export default CartWidget;
